d2afd7049faa2c19e5bcfe4979283f46e0199d83
Kyu hyunSung <107687577+dmp100@users.noreply.github.com>
2023-05-06 16:46:55 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.39 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int num) {
+    // 리턴할 값은 메모리를 동적 할당해주세요
+    char* answer = (char*)malloc(5);
+    answer=(num%2==0)?"Even":"Odd";
+    //num을 2로 나누어 0으로 떨어지면 짝,아님 홀
+    
+    
+    return answer;
+}
