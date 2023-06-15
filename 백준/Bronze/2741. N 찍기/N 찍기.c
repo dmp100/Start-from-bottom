26f067afa65e9a8de1d7d62002ec974b84b350ff
Kyu hyunSung <107687577+dmp100@users.noreply.github.com>
2023-06-15 21:32:13 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>    // strlen 함수가 선언된 헤더 파일
+#pragma warning(disable : 4996)
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	for (int i = 1; i <=N; i++)
+	{
+		
+		printf("%d\n", i);
+
+
+
+	}
+}
