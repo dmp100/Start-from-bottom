629efdbdf69bba7ffce558d68d90f4e1bd65f1d1
Kyu hyunSung <107687577+dmp100@users.noreply.github.com>
2023-06-18 16:01:15 +0900

    Update 기찍 N.c

--- 백준/Bronze/2742. 기찍 N/기찍 N.c
+++ 백준/Bronze/2742. 기찍 N/기찍 N.c
@@ -13,3 +13,18 @@ int main()
 	
 
 }
+
+
+//다른해답
+
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = n; i > 0; --i) //그냥 N부터 시작하게 뒤집어버림
+	{
+		printf("%d\n", i);
+	}
+	return 0;
+}
