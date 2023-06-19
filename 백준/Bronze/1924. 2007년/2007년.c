5fe3938f97750428e291e7f7956aafd7e50c9878
Kyu hyunSung <107687577+dmp100@users.noreply.github.com>
2023-06-19 21:52:33 +0900

    [Bronze I] Title: 2007년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.c
@@ -0,0 +1,11 @@
+int main()
+{
+	int x, y;
+	int month[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	char* day[7] = { "SUN","MON","TUE","WED","THU","FRI","SAT" };
+	scanf("%d %d", &x, &y);
+	for (int i = 0; i < x - 1; i++)
+		y += month[i];
+	printf("%s", day[y % 7]);
+
+}
