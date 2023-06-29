e4afcc59b8e91a4c5820db00f773c47977b4e030
Kyu hyunSung <107687577+dmp100@users.noreply.github.com>
2023-06-29 16:54:00 +0900

    [Bronze III] Title: 최소, 최대, Time: 176 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -0,0 +1,25 @@
+// 1.for문과 if문으로 풀기
+#include <stdio.h>
+
+int main()
+{
+	int n, input, min = 1000000, max = -1000000; // 최대값, 최소값 설정 주의
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) { // n개의 정수만큼 최소, 최대 비교
+		scanf("%d", &input);
+
+		if (input >= max) {
+			max = input;
+		}
+
+		if (input <= min) {
+			min = input;
+		}
+	}
+
+	printf("%d %d", min, max);
+
+	return 0;
+}
