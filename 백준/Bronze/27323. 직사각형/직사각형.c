f18099fac8fc7b2e115effac80acfe8b6111ecac
Kyu hyunSung <107687577+dmp100@users.noreply.github.com>
2023-07-05 19:13:23 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>    // strlen 함수가 선언된 헤더 파일
+#include <time.h>
+#pragma warning(disable : 4996)
+
+
+
+
+int main()
+{
+	int A,B;
+	scanf("%d %d", &A,&B);
+
+	printf("%d", A * B);
+
+	
+	
+
+}
