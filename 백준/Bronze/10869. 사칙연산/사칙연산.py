9e37514eeb6be4fd43bb5a916cc2ec6294d23588
Kyu hyunSung <107687577+dmp100@users.noreply.github.com>
2023-11-18 18:03:56 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,7 +1,6 @@
-a,b = map(int,input().split())
-
-print(a+b)
-print(a-b)
-print(a*b)
-print(a//b)
-print(a%b)
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(int(a / b))
+print(a % b)
